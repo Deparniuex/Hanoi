99729460c0de9301300b1e3e05607523a4aeeae5
Deparniuexx <dep2and@gmail.com>
2023-08-17 20:26:03 +0700

    Added switch to Game Activity

--- app/src/main/java/com/hanoitower/MenuActivity.java
+++ app/src/main/java/com/hanoitower/MenuActivity.java
@@ -1,14 +1,29 @@
 package com.hanoitower;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
+        this.findViewById(R.id.menu_game_start).setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.menu_game_start) {
+            Intent intent = new Intent(this, GameActivity.class);
+            intent.putExtra("rings", 3);
+            startActivity(intent);
+            finish();
+        }
     }
 }
